7e16d392455ea6eb6ffed7d6254e36850c683d15
Danuka Aluthge <shrml.lthg@gmail.com>
2024-12-05 15:08:25 +1100

    Add files via upload

---
+++ depth-chart/src/main/java/com/sportsbet/fanduel/nfl/depth_chart/controller/DepthChartApplication.java
@@ -0,0 +1,15 @@
+package com.sportsbet.fanduel.nfl.depth_chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepthChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepthChartApplication.class, args);
+	}
+
+
+
+}
